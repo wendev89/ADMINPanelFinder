f29625fb924a66666fa45c081ab83610a281293b
wendev89 <47571180+wendev89@users.noreply.github.com>
2019-09-16 23:53:00 +0700

    Create BuildConfig.java

---
+++ java/com/adminpanelfinder/BuildConfig.java
@@ -0,0 +1,38 @@
+package com.adminpanelfinder;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = true;
+
+    /*  JADX ERROR: NullPointerException in pass: u
+        java.lang.NullPointerException
+        	at e.b.d.g.b.u$a.a(SourceFile:119)
+        	at e.b.d.g.b.u$a.a(SourceFile:92)
+        	at e.b.d.g.b.u$a.a(SourceFile:52)
+        	at e.b.d.g.b.w.a(SourceFile:23)
+        	at e.b.d.g.b.f.b(SourceFile:54)
+        	at e.b.d.g.b.f.a(SourceFile:58)
+        	at e.b.d.g.b.g.accept(Unknown Source)
+        	at d.b.a.c.l.f.a(SourceFile:85)
+        	at e.b.d.g.b.f.b(SourceFile:58)
+        	at e.b.d.g.b.f.a(SourceFile:19)
+        	at e.b.d.g.b.u.a(SourceFile:30)
+        	at e.b.d.g.i.a(SourceFile:28)
+        	at e.b.d.g.i.b(SourceFile:15)
+        	at e.b.d.g.k.accept(Unknown Source)
+        	at d.b.a.c.l.f.a(SourceFile:85)
+        	at e.b.d.g.i.a(SourceFile:15)
+        	at e.b.b.a(SourceFile:33)
+        	at e.a.d.a(SourceFile:402)
+        	at e.a.f.a(SourceFile:62)
+        	at e.a.d.a(SourceFile:296)
+        	at e.a.e.run(Unknown Source)
+        */
+    public BuildConfig() {
+        /*
+        r0 = this;
+        r0.<init>();
+        return;
+        */
+        throw new UnsupportedOperationException("Method not decompiled: com.adminpanelfinder.BuildConfig.<init>():void");
+    }
+}
